30ef1cfa60f61fac729980602cd887c1ec77c662
Johnson Page <jwpage@gmail.com>
2010-06-28 13:21:40 +1000

    Moved namespace to the config, along with a URL for xhprof

--- external/footer.php
+++ external/footer.php
@@ -1,13 +1,13 @@
 <?php
 if (extension_loaded('xhprof') && $_xhprof['doprofile'] === true) {
-    $profiler_namespace = 'myapp';  // namespace for your application
+    $profiler_namespace = $_xhprof['namespace'];  // namespace for your application
     $xhprof_data = xhprof_disable();
     $xhprof_runs = new XHProfRuns_Default();
     $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace, null, $_xhprof);
     if ($_xhprof['display'] === true)
     {
         // url to the XHProf UI libraries (change the host name and path)
-        $profiler_url = sprintf('http://xh.example.com/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
+        $profiler_url = sprintf($_xhprof['url'].'/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
         echo '<a href="'. $profiler_url .'" target="_blank">Profiler output</a>';    
     }
 }
