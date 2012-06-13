59dace89683d9bcf1c93c98e1fa346a2e285ff67
bertrandmalet <malet.bertrand@gmail.com>
2012-06-13 23:13:16 +0200

    Fix PDO escaping which caused error on insert

---
+++ xhprof_lib/utils/Db/Mysql.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * When setting the `id` column, consider the length of the prefix you're specifying in $this->prefix
+ *
+ *
+ CREATE TABLE `details` (
+ `id` char(17) NOT NULL,
+ `url` varchar(255) default NULL,
+ `c_url` varchar(255) default NULL,
+ `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+ `server name` varchar(64) default NULL,
+ `perfdata` MEDIUMBLOB,
+ `type` tinyint(4) default NULL,
+ `cookie` BLOB,
+ `post` BLOB,
+ `get` BLOB,
+ `pmu` int(11) unsigned default NULL,
+ `wt` int(11) unsigned default NULL,
+ `cpu` int(11) unsigned default NULL,
+ `server_id` char(3) NOT NULL default 't11',
+ `aggregateCalls_include` varchar(255) DEFAULT NULL,
+ PRIMARY KEY  (`id`),
+ KEY `url` (`url`),
+ KEY `c_url` (`c_url`),
+ KEY `cpu` (`cpu`),
+ KEY `wt` (`wt`),
+ KEY `pmu` (`pmu`),
+ KEY `timestamp` (`timestamp`)
+ ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ */
+
+require_once XHPROF_LIB_ROOT.'/utils/Db/Abstract.php';
+class Db_Mysql extends Db_Abstract
+{
+    
+    public function connect()
+    {
+        $linkid = mysql_connect($this->config['dbhost'], $this->config['dbuser'], $this->config['dbpass']);
+        if ($linkid === FALSE)
+        {
+            xhprof_error("Could not connect to db");
+            throw new Exception("Unable to connect to database");
+            return false;
+        }
+        $this->query("SET NAMES utf8");
+        mysql_select_db($this->config['dbname'], $linkid);
+        $this->linkID = $linkid;
+    }
+    
+    public function query($sql)
+    {
+        return mysql_query($sql);
+    }
+    
+    public static function getNextAssoc($resultSet)
+    {
+        return mysql_fetch_assoc($resultSet);
+    }
+    
+    public function escape($str)
+    {
+        return mysql_real_escape_string($str);
+    }
+    
+    public function affectedRows()
+    {
+        return mysql_affected_rows($this->linkID);
+    }
+    
+    public static function unixTimestamp($field)
+    {
+        return 'UNIX_TIMESTAMP('.$field.')';
+    }
+    
+    public static function dateSub($days)
+    {
+        return 'DATE_SUB(CURDATE(), INTERVAL '.$days.' DAY)';
+    }
+}
---
+++ xhprof_lib/utils/Db/Pdo.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ * When setting the `id` column, consider the length of the prefix you're specifying in $this->prefix
+ *
+ *
+ CREATE TABLE `details` (
+ `id` char(17) NOT NULL,
+ `url` varchar(255) default NULL,
+ `c_url` varchar(255) default NULL,
+ `timestamp` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+ `server name` varchar(64) default NULL,
+ `perfdata` MEDIUMBLOB,
+ `type` tinyint(4) default NULL,
+ `cookie` BLOB,
+ `post` BLOB,
+ `get` BLOB,
+ `pmu` int(11) unsigned default NULL,
+ `wt` int(11) unsigned default NULL,
+ `cpu` int(11) unsigned default NULL,
+ `server_id` char(3) NOT NULL default 't11',
+ `aggregateCalls_include` varchar(255) DEFAULT NULL,
+ PRIMARY KEY  (`id`),
+ KEY `url` (`url`),
+ KEY `c_url` (`c_url`),
+ KEY `cpu` (`cpu`),
+ KEY `wt` (`wt`),
+ KEY `pmu` (`pmu`),
+ KEY `timestamp` (`timestamp`)
+ ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ */
+
+require_once XHPROF_LIB_ROOT.'/utils/Db/Abstract.php';
+class Db_Pdo extends Db_Abstract
+{
+    protected $curStmt;
+    
+    public function connect()
+    {
+        $connectionString = $this->config['dbtype'] . ':host=' . $this->config['dbhost'] . ';dbname=' . $this->config['dbname'];
+        $db = new PDO($connectionString, $this->config['dbuser'], $this->config['dbpass']);
+        if ($db === FALSE)
+        {
+            xhprof_error("Could not connect to db");
+            $run_desc = "could not connect to db";
+            throw new Exception("Unable to connect to database");
+            return false;
+        }
+        $this->db = $db;
+        $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }
+    
+    public function query($sql)
+    {
+        $this->curStmt = $this->db->query($sql);
+        return $this->curStmt;
+    }
+    
+    public static function getNextAssoc($resultSet)
+    {
+        return $resultSet->fetch();
+    }
+    
+    public function escape($str)
+    {
+        $str = $this->db->quote($str);
+        //Dirty trick, PDO::quote add quote around values (you're beautiful => 'you\'re beautiful')
+        // which are already added in xhprof_runs.php
+        $str = substr($str, 0, -1);
+        $str = substr($str, 1);
+        return $str;
+    }
+    
+    public function affectedRows()
+    {
+        if ($this->curStmt === false) {
+            return 0;
+        }
+        return $this->curStmt->rowCount();
+    }
+    
+    public static function unixTimestamp($field)
+    {
+        return 'UNIX_TIMESTAMP('.$field.')';
+    }
+    
+    public static function dateSub($days)
+    {
+        return 'DATE_SUB(CURDATE(), INTERVAL '.$days.' DAY)';
+    }
+}
