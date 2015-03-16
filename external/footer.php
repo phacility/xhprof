916fb519c741678081476765145f85748ae1a57f
Vladimir Polischuk <vladimir.polischuk@eltrino.com>
2015-03-16 14:11:14 +0200

    fix typo error

--- external/footer.php
+++ external/footer.php
@@ -12,7 +12,7 @@ if (extension_loaded('xhprof') && $_xhprof['doprofile'] === true) {
     $xhprof_data = xhprof_disable();
     $xhprof_runs = new XHProfRuns_Default();
     $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace, null, $_xhprof);
-    if ($_xhprof['display'] === true && PHP_SAPI != 'cli' && isset($isAjax))
+    if ($_xhprof['display'] === true && PHP_SAPI != 'cli' && !isset($isAjax))
     {
         // url to the XHProf UI libraries (change the host name and path)
         $profiler_url = sprintf($_xhprof['url'].'/index.php?run=%s&source=%s', $run_id, $profiler_namespace);
