231ec6f186e091d0a86a01a26749dd69f8bc44b3
sunan <327369214@qq.com>
2015-09-04 00:04:50 +0800

    add url

--- examples/sample.php
+++ examples/sample.php
@@ -37,8 +37,9 @@ $xhprof_runs = new XHProfRuns_Default();
 // save the run under a namespace "xhprof_foo"
 $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_foo");
 
+
 echo "---------------\n".
      "Assuming you have set up the http based UI for \n".
      "XHProf at some address, you can view run at \n".
-     "http://<xhprof-ui-address>/index.php?run=$run_id&source=xhprof_foo\n".
+     "<a href='http://".$_SERVER['HTTP_HOST']."/xhprof_html/index.php?run=$run_id&source=xhprof_foo'>click here</a>\n".
      "---------------\n";
